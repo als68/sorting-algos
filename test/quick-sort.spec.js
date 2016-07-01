79d7416a3fba14a7649d9a46d063e2bf1e8c67e7
Andy <suhandyl@gmail.com>
2016-06-30 15:19:05 -1000

    First commit, bubble and quick sorts functional

--- test/quick-sort.spec.js
+++ test/quick-sort.spec.js
@@ -13,23 +13,7 @@ describe('The algorithm should quick sort', function(){
     expect(qSort).to.be.a('function');
   });
 
-/*  it('the 1-2 array should be sorted correctly', function(){
-    expect(qSort([2, 1])).to.deep.equal([1, 2]);
-  });
-
-  it('the 1-5 array should be sorted correctly', function(){
-    expect(qSort([5, 4, 3, 2, 1])).to.deep.equal([1, 2, 3, 4, 5]);
-  });
-
-  it('the 1-10 array should be sorted correctly', function(){
-    expect(qSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-  });*/
-
   it('the random array should be sorted correctly', function(){
-    expect(qSort([3, 5, 1, 7, 8, 19, 4, 22, 9, 2])).to.deep.equal([1, 2, 3, 4, 5, 7, 8, 9, 19, 22]);
-  });
-
-    it('the random array should be sorted correctly', function(){
     expect(qSort([4, 3, 5, 1, 7, 7, 8, 19, 4, 22, 9, 2, 99, 100])).to.deep.equal([1, 2, 3, 4, 5, 7, 8, 9, 19, 22]);
   });
 
